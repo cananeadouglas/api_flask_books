1617e0d65e83b7127548c9d763ebb742819769be
Douglas Cananéa <cananeadouglas@gmail.com>
2022-11-12 16:06:30 -0300

    get, post, put, delete in python

---
+++ src/db.py
@@ -0,0 +1,18 @@
+livros = [
+    
+    {
+        'id': 1,
+        'title': 'A única coisa',
+        'autor': 'douglas'
+    },
+    {
+        'id': 2,
+        'title': 'Fédon - Imortalidade da Alma',
+        'autor': 'Platão'
+    },
+    {
+        'id': 3,
+        'title': 'é o bicho',
+        'autor': 'Cynthina'
+    }
+]
